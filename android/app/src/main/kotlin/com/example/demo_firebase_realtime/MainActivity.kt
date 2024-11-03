4237fecb68731b3388ebd120dd3074aba5b440f6
La Thiên Bảo <jukute2000@gmail.com>
2024-11-03 21:17:29 +0700

    commit init

---
+++ android/app/src/main/kotlin/com/example/demo_firebase_realtime/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.demo_firebase_realtime
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:demo_firebase_realtime/view/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeView(),
+    );
+  }
+}
